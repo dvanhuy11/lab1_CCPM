754e1e00daefdd0202d9bd704e98aabb6026d466
HUY DINH <dvanhuy11@gmail.com>
2024-03-11 16:09:04 +0700

    add login sucees

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-// Serve static files from the 'public' directory
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Define a route handler for GET requests to the root URL ("/")
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+// Xử lý yêu cầu POST từ form đăng nhập
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    // Thay thế với cơ chế xác thực thực tế
+    if (username === "user" && password === "pass") {
+        // Chuyển hướng đến trang đăng nhập thành công
+        res.redirect('/success.html');
+    } else {
+        // Chuyển hướng đến trang đăng nhập với thông báo lỗi
+        res.send('Login Failed');
+    }
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
